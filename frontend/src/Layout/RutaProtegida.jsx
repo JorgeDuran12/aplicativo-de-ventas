b76f22861a8fa16e1533508b917930b371ecb35c
JorgeDuran12 <duranolivaresjorgeluis1@gmail.com>
2023-03-26 12:40:12 -0500

    first commit

---
+++ frontend/src/Layout/RutaProtegida.jsx
@@ -0,0 +1,16 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import Navbar from '../components/Navbar.jsx';
+
+const RutaProtegida = () => {
+    const { auth, cargando } = useAuth();
+    console.log(auth)
+    if (cargando) return 'cargando...'
+    return (
+        <div className="h-full">
+            {/* <Navbar /> */}
+            <Outlet />
+        </div>
+    )
+}
+export default RutaProtegida
